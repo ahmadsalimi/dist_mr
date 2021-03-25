7bd7a296cfad1755c91eff5cbafda047708df8f0
ahmadsalimi <ahsa9978@gmail.com>
2021-03-25 16:08:48 +0430

    Make worker wait for driver to start.

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import asyncio
 import grpc
@@ -7,12 +8,24 @@ from driver_service_pb2 import HelloRequest
 
 
 async def run() -> None:
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
-        stub = DriverServiceStub(channel)
-        response = await stub.SayHello(HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+    available = True
+    while True:
+        try:
+            async with grpc.aio.insecure_channel('localhost:50051') as channel:
+                stub = DriverServiceStub(channel)
+                response = await stub.SayHello(HelloRequest(name='you'))
+                available = True
+            print("Greeter client received: " + response.message)
+        except grpc.aio._call.AioRpcError as e:
+            if not e._code == grpc.StatusCode.UNAVAILABLE:
+                raise
+            if available:
+                logging.info('driver is unavailable')
+            available = False
+            time.sleep(1)
+            
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(run())
